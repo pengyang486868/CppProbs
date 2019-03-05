d235cf9da5136e94a36003ab426b2d7647227395
yang peng <854525261@qq.com>
2019-03-05 11:41:14 +0800

    upperbound

--- CppLesson.Prob750UpperBound/CppLesson.Prob750UpperBound.cpp
+++ CppLesson.Prob750UpperBound/CppLesson.Prob750UpperBound.cpp
@@ -1,10 +1,12 @@
 ﻿#include "pch.h"
-#include <iostream>
 
 #define N 1000000
 using namespace std;
+
 long long a, n, m, i, j, s[N], d, ans[N];
-int main() {
+
+void OldMethod() {
+	freopen("input.txt", "r", stdin);
 	for (a = 0; a < N; a++)
 		ans[a] = 0;
 	cin >> n >> m;
@@ -17,6 +19,24 @@ int main() {
 		}
 	}
 
+	freopen("output.txt", "w", stdout);
 	for (j = 1; j <= m; j++) cout << ans[j] << endl;
+}
+
+
+int main() {
+	//OldMethod();
+	freopen("input.txt", "r", stdin);
+
+	cin >> n >> m;
+	for (i = 1; i <= n; i++) cin >> s[i];
+	sort(s, s + n);
+
+	freopen("output.txt", "w", stdout);
+	for (j = 1; j <= m; j++) {
+		cin >> d;
+		cout << upper_bound(s, s + n, d) - s << endl;
+	}
+
 	return 0;
 }
