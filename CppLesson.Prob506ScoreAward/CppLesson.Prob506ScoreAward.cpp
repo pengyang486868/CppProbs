008f49951bba61c26e8cc5e76d5e33219a3bf6eb
pengyang <pengyang@PENGY>
2019-03-17 14:26:23 +0800

    506

---
+++ CppLesson.Prob506ScoreAward/CppLesson.Prob506ScoreAward.cpp
@@ -0,0 +1,48 @@
+﻿#include "pch.h"
+#include<iostream>
+
+using namespace std;
+
+struct student {
+	int id, chinese, math, english, total;
+};
+
+bool compare(student x, student y)
+{
+	if (x.total == y.total)
+		return x.chinese > y.chinese;
+	return x.total > y.total;
+}
+
+// main
+int main()
+{
+	int stuCount;
+	student stu[300];
+
+	freopen("input.txt", "r", stdin);
+	int curChi, curMath, curEng;
+	cin >> stuCount;
+	for (int i = 0; i < stuCount; i++) {
+		cin >> curChi >> curMath >> curEng;
+		stu[i].id = i + 1;
+		stu[i].chinese = curChi;
+		stu[i].math = curMath;
+		stu[i].english = curEng;
+		stu[i].total = curChi + curMath + curEng;
+	}
+
+	sort(stu, stu + stuCount, compare);
+
+
+	freopen("output.txt", "w", stdout);
+	for (int i = 0; i < 5; i++) {
+		if (i == 4)
+			cout << stu[i].id << " " << stu[i].total;
+		else
+			cout << stu[i].id << " " << stu[i].total << endl;
+	}
+
+	return 0;
+}
+
