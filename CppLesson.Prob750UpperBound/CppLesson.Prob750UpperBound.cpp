3d3629802a917e229aaa94338cad62074da9df3c
pengyang <pengyang@PENGY>
2019-03-18 00:12:39 +0800

    750pass

--- CppLesson.Prob750UpperBound/CppLesson.Prob750UpperBound.cpp
+++ CppLesson.Prob750UpperBound/CppLesson.Prob750UpperBound.cpp
@@ -35,13 +35,22 @@ int main() {
 	freopen("input.txt", "r", stdin);
 
 	cin >> n >> m;
-	for (i = 1; i <= n; i++) cin >> s[i];
+	for (i = 0; i < n; i++) cin >> s[i];
 	sort(s, s + n);
 
 	freopen("output.txt", "w", stdout);
 	for (j = 1; j <= m; j++) {
 		cin >> d;
 		cout << upper_bound(s, s + n, d) - s;
+		/*if (s[n - 1] <= d)cout << n;
+		else {
+			for (int k = 0;k < n - 1;k++) {
+				if (s[k] <= d && s[k + 1] > d) {
+					cout << k+1;
+					break;
+				}
+			}
+		}*/
 		if (j < m)
 			cout << endl;
 	}
